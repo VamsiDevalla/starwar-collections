75ff2b6640cfbfb03df012757b09e7695551b390
vamsidevalla <vammichanti40312@gmail.com>
2021-12-06 05:11:13 -0500

    implementing basic nav bar

---
+++ ui/src/app/top-nav/top-nav.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../user/auth.service';
+
+@Component({
+  selector: 'swc-top-nav',
+  templateUrl: './top-nav.component.html',
+  styleUrls: ['./top-nav.component.scss'],
+})
+export class TopNavComponent {
+  get isLoggedIn() {
+    return this.userService.isAuthenticated;
+  }
+  get userName() {
+    return this.userService.user?.name;
+  }
+  constructor(private userService: AuthService, private router: Router) {}
+
+  logOut() {
+    this.userService.redirectUrl = this.router.routerState.snapshot.url;
+    this.userService.logout();
+  }
+}
