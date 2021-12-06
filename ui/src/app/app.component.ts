737780ea3fbdb701daaf2b34beeda240ca3eb180
vamsidevalla <vammichanti40312@gmail.com>
2021-12-06 16:27:36 -0500

    showing loading on boot up and  route changes

--- ui/src/app/app.component.ts
+++ ui/src/app/app.component.ts
@@ -1,10 +1,30 @@
 import { Component } from '@angular/core';
+import { Router, Event, NavigationStart, NavigationEnd, NavigationCancel, NavigationError } from '@angular/router';
 
 @Component({
   selector: 'swc-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'ui';
+  nextRouteLoading = false;
+  constructor(private router: Router) {
+    router.events.subscribe({
+      next: (routerEvent: Event) => this.setNextRouteLoading(routerEvent),
+    });
+  }
+
+  setNextRouteLoading(routerEvent: Event) {
+    if (routerEvent instanceof NavigationStart) {
+      this.nextRouteLoading = true;
+    }
+
+    if (
+      routerEvent instanceof NavigationEnd ||
+      routerEvent instanceof NavigationCancel ||
+      routerEvent instanceof NavigationError
+    ) {
+      this.nextRouteLoading = false;
+    }
+  }
 }
